644bc1c148f1ac6f02de876904112b74b9a7b705
Hiroki Katagiri <hirokata3941@gmail.com>
2020-07-28 13:08:10 +0900

    second commit

---
+++ js/app.js
@@ -0,0 +1,28 @@
+$(function(){
+
+  $(window).scroll(function(){
+      $('.js-title-color').toggleClass('white', $(this).scrollTop() > $('#news').offset().top);
+      $('.menu-link').toggleClass('white', $(this).scrollTop() > $('#news').offset().top);
+  });
+
+  $('.menu-trigger').on('click', function(){
+    $('.js-toggle-menu').toggleClass('active');
+    $('.js-toggle-humburger').toggleClass('active');
+  });
+
+  $('.menu-link').on('click', function(){
+    $('.js-toggle-menu').toggleClass('active');
+  });
+
+  $(window).scroll(function(){
+    var wHeight = $(window).height();
+    var scrollAmount = $(window).scrollTop();
+
+    $('.scrollAnime').each(function(){
+      var targetPosition = $(this).offset().top;
+      if(scrollAmount > targetPosition - wHeight + 60){
+        $(this).addClass('fadeIndown');
+      }
+    });
+  });
+});
